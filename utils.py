a5e8f2bbe08f738bea41b486383ef03a48402565
p-and14 <p-and14@yandex.ru>
2022-04-23 17:47:50 +0300

    Продокументировал функции в utils.py Проставил пробелы до и полсе переменных в файлах html

--- utils.py
+++ utils.py
@@ -2,18 +2,35 @@ import json
 
 
 def load_candidates_from_json(path):
+    """
+    Загружает список кандидатов из json файла
+    :param path: json файл со списком кандидатов
+    :return: Список кандидатов
+    """
     with open(path, "r") as file:
         candidates_list = json.load(file)
     return candidates_list
 
 
 def get_candidate(candidates, candidate_id):
+    """
+    Получает одного кандидата из списка по id
+    :param candidates: Список кандидатов
+    :param candidate_id: id кандидата
+    :return: Возвращает одного кандидата
+    """
     for candidate in candidates:
         if candidate["id"] == candidate_id:
             return candidate
 
 
 def get_candidates_by_name(candidates, candidate_name):
+    """
+    Получает список кандидатов по имени из общего списка
+    :param candidates: Список кандидатов
+    :param candidate_name: Имя кандидата
+    :return: Список кандидатов по имени
+    """
     candidates_by_name = []
     for candidate in candidates:
         if candidate_name.lower() in candidate["name"].lower():
@@ -22,6 +39,12 @@ def get_candidates_by_name(candidates, candidate_name):
 
 
 def get_candidates_by_skill(candidates, skill_name):
+    """
+    Получает список кандидатов по скиллу из общего списка
+    :param candidates: Список кандидатов
+    :param skill_name: Название скилла
+    :return: Список кандидатов по скиллу
+    """
     candidates_by_skill = []
     for candidate in candidates:
         candidate_skills = candidate["skills"].lower().split(", ")
